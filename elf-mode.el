78abd88aba7509d4b3326550407e39aaf08f8d7f
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2019-04-30 13:44:05 +0300

    Add tabulated-list-mode based interface

--- elf-mode.el
+++ elf-mode.el
@@ -33,18 +33,68 @@
 
 ;;; Code:
 
+(require 'cl-macs)
+(require 'seq)
+(require 'subr-x)
+(require 'thingatpt)
+
+(defvar-local elf-mode-disassemble-command
+  "objdump --disassemble=%s %s")
+
+(defun elf-mode-set-disassemble-command (s)
+  (interactive "sDisassemble command: ")
+  (unless (string-empty-p s)
+    (setq elf-mode-disassemble-command s)))
+
+(defun elf-mode-disassemble (marker)
+  (interactive)
+  (let* ((symbol (save-excursion
+                   (goto-char marker)
+                   (word-at-point)))
+         (buffer-name (format "%s(%s)" (buffer-name) symbol))
+         (command (format elf-mode-disassemble-command symbol (buffer-file-name))))
+    (with-current-buffer (pop-to-buffer buffer-name)
+      (shell-command command (current-buffer))
+      (flush-lines "^[[:space:]]*$" (point-min) (point-max))
+      (set-buffer-modified-p nil)
+      (asm-mode)
+      (read-only-mode))))
+
 (defvar elf-mode-command "readelf --syms -W %s"
   "The shell command to use for `elf-mode'.")
 
+(defun elf-mode-entries ()
+  (interactive)
+  (let* ((command (format elf-mode-command (buffer-file-name)))
+         (output (shell-command-to-string command))
+         (lines (seq-drop (split-string output "\n" t) 2)))
+    (mapcar
+     (lambda (line)
+       (let ((entry (apply #'vector (split-string line "[[:space:]]+" t))))
+         (aset entry 0 (format "%8s" (aref entry 0)))
+         (aset entry 2 (format "%8s" (aref entry 2)))
+         (cl-case (length entry)
+           (7 (setq entry (vconcat entry [""])))
+           (8 (when (string= "FUNC" (aref entry 3))
+                (aset entry 7 `(,(aref entry 7) . (action elf-mode-disassemble)))))
+           (otherwise (error "Oops")))
+         (list (aref entry 0) entry)))
+     lines)))
+
+(defconst elf-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" st)
+    st))
+
 ;;;###autoload
-(define-derived-mode elf-mode fundamental-mode "Elf"
+(define-derived-mode elf-mode tabulated-list-mode "Elf"
   "TODO"
-  (let ((inhibit-read-only t))
-    (erase-buffer)
-    (insert (shell-command-to-string
-             (format elf-mode-command (buffer-file-name))))
-    (set-buffer-modified-p nil)
-    (read-only-mode 1)))
+  :syntax-table elf-mode-syntax-table
+  (setq tabulated-list-format [("Num" 8 t) ("Value" 8 t) ("Size"  8 t) ("Type"  8 t) ("Bind"  8 t) ("Vis"   8 t) ("Index" 8 t) ("Name"  8 t)]
+        tabulated-list-entries #'elf-mode-entries)
+  (define-key elf-mode-map (kbd "C-c C-c") #'elf-mode-set-disassemble-command)
+  (tabulated-list-init-header)
+  (tabulated-list-revert))
 
 ;;;###autoload
 (add-to-list 'magic-mode-alist '("\177ELF" . elf-mode))
